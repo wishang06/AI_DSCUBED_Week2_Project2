ce14ec303bc18b99250475c47a7927716c8fd510
pranavjayanty <pranav.jayanty@gmail.com>
2025-07-05 02:34:38 +1000

    tiny

--- database/bronze/pipelines/notion_committee.py
+++ database/bronze/pipelines/notion_committee.py
@@ -5,8 +5,8 @@ from sqlalchemy.engine import Engine
 from pathlib import Path
 from typing import Optional
 import sys
-sys.path.append(str(Path(__file__).parent.parent / 'src'))
-from extractor.notion_extractor import NotionExtractor
+# sys.path.append(str(Path(__file__).parent.parent / 'src'))
+# from extractor.notion_extractor import NotionExtractor
 # from utils.pipeline import Pipeline
 
 def main():
@@ -14,19 +14,18 @@ def main():
     NOTION_API_KEY = os.getenv('NOTION_API_KEY')
     NOTION_USERS_DATABASE_ID = os.getenv('NOTION_USERS_DATABASE_ID')
     notion_extractor = NotionExtractor(NOTION_API_KEY, NOTION_USERS_DATABASE_ID)
-    print(notion_extractor.fetch_user_data())
-    # notion_pipeline = Pipeline(
-    #     ddl_filepath = 'ddl/create_bronze_table.sql',
-    #     table_name = 'notion_users',
-    # )
-    # if notion_extractor.recreate_table:
-    #     notion_pipeline.create_table()
-    # notion_pipeline.ingest_from_df(
-    #     notion_extractor.transform_user_data(
-    #         notion_extractor.fetch_user_data()
-    #     )
-    # )
-    # notion_pipeline.test_run_status() 
+    notion_pipeline = Pipeline(
+        ddl_filepath = 'ddl/create_bronze_table.sql',
+        table_name = 'notion_users',
+    )
+    if notion_extractor.recreate_table:
+        notion_pipeline.create_table()
+    notion_pipeline.ingest_from_df(
+        notion_extractor.transform_user_data(
+            notion_extractor.fetch_user_data()
+        )
+    )
+    notion_pipeline.test_run_status() 
 
 if __name__ == "__main__":
     main()
